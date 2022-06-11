b818a2c38eb84e80ef57c3a58d6d913c2b5de263
Senid Alihodzic <alihodzic_senid@yahoo.com>
2022-06-11 09:55:39 +0200

    Before responsive

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
 // JQuery Code
 $(document).ready(function () {
 
+    // Side menu slide-in-left
     $("#side-menu").hover(function(){
         $("#side-menu").addClass("opened");
         $(".side-nav").removeClass("justify-content-center");
     }, function () {
         $(".side-nav").addClass("justify-content-start");
         $("#side-menu").removeClass("opened");
+    });
+
+    // Header Shadow on scroll
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 0) {
+            $("#site-header").addClass("scrolled");
+        }
+        else {
+            $("#site-header").removeClass("scrolled");
+        }
     });
 
 });
